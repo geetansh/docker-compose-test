d27c2f3ba2f7239d1da48454d4d1fa6b413adf5e
Geetansh Jindal <geetansh@hackreports.com>
2019-06-18 12:46:31 +0530

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,12 @@
+if [ -d /home/ec2-user/docker ]; then
+  sudo rm -R /home/ec2-user/docker
+  mkdir /home/ec2-user/docker
+else
+   mkdir /home/ec2-user/docker
+
+fi
+
+
 yum install -y docker docker-compose
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -6,14 +15,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-if [ -d /home/ec2-user/docker ]; then
-  sudo rm -R /home/ec2-user/docker
-  mkdir /home/ec2-user/docker
-fi
-
-
-
-cd /home/ec2-user/docker;
+cd /home/ec2-user/docker
 
 pwd
 
